3b048d8415de0d21ee83ed3924c1a4a970e9bcd1
Ali Haydar Kaplan <alihaydar.kaplan@delaware.pro>
2026-03-18 13:03:03 +0000

    Run the service with real remote delegation
    cds watch --profile sandbox , normal cds w fetches from local
    
    https://github.com/SAP-samples/cap-service-integration-codejam/tree/main/exercises/10-run-with-real-remote-delegation

--- incidents/srv/incidents-service.js
+++ incidents/srv/incidents-service.js
@@ -4,7 +4,7 @@ module.exports = cds.service.impl(async function () {
     const S4bupa = await cds.connect.to('API_BUSINESS_PARTNER')
 
     this.on('READ', 'Customers', (req) => {
-        console.log('>> delegating to remote service...')
+        console.log('>> delegating to remote service...') 
         return S4bupa.run(req.query)
     })
 })
